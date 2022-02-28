f62c519d6544a96147d089cc54676616d27deb50
Vincenzo Salvati <vincenzosalvati@hotmail.it>
2022-02-28 18:04:17 +0100

    Extract faces from images

--- FriendRecognize/FriendRecognize.py
+++ FriendRecognize/FriendRecognize.py
@@ -1,34 +1,59 @@
+import os
+
 import cv2 as cv
-  
-# Load the cascade  
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')  
+import dlib
+from tqdm import tqdm
 
-video_capture = cv.VideoCapture(0)
+from FaceAligner import FaceAligner
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv.imread(os.path.join(folder, filename))
+        if img is not None:
+            images.append(img)
+    return images
 
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv.CASCADE_SCALE_IMAGE
-    )
 
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+if __name__ == '__main__':
+    faceProto = "opencv_face_detector.pbtxt"
+    faceModel = "opencv_face_detector_uint8.pb"
+    faceNet = cv.dnn.readNet(faceModel, faceProto)
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    fa = FaceAligner(predictor, desiredFaceWidth=224, desiredFaceHeight=224)
 
-    # Display the resulting frame
-    cv.imshow('Video', frame)
+    face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    source_images = "./source"
 
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+    if not os.path.exists(source_images):
+        print("There are no images to detect")
+        exit(1)
 
-# When everything is done, release the capture
-video_capture.release()
-cv.destroyAllWindows()
+    destination_images = "./destination"
+    if not os.path.exists(destination_images):
+        os.makedirs(destination_images)
+
+    padding = 20
+    i = 0
+    for image in tqdm(load_images_from_folder(source_images), "Detect faces from images"):
+        scale_factor = 2
+        new_height = int(image.shape[0] * scale_factor)
+        new_width = int(image.shape[1] * scale_factor)
+        dimensions = (new_width, new_height)
+        image = cv.resize(image, dimensions, interpolation=cv.INTER_LANCZOS4)
+
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(50, 50),
+            flags=cv.CASCADE_SCALE_IMAGE
+        )
+
+        for (x, y, w, h) in faces:
+            cv.imwrite(destination_images + "/" + str(i) + ".jpg",
+                       image[y - padding:y + h + padding, x - padding:x + w + padding])
+            i += 1

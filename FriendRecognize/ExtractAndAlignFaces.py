ea0099dbba7a95dff4a7030510deeef215a0fc3f
Vincenzo Salvati <vincenzosalvati@hotmail.it>
2022-03-05 22:21:30 +0100

    Bug Fix

---
+++ FriendRecognize/ExtractAndAlignFaces.py
@@ -0,0 +1,90 @@
+import os
+import shutil
+
+import cv2 as cv
+import dlib
+import yaml
+from tqdm import tqdm
+
+from FriendRecognize.utils.Filtering import align_face_from, crop_face_from
+from FriendRecognize.utils.UsefulMethods import load_images_from
+
+
+def get_face_cascade_path(config):
+    return config['libs']['face_cascade']
+
+
+def get_predictor_path(config):
+    return config['libs']['predictor']
+
+
+def get_source_path(config):
+    return config['data']['source_images']
+
+
+def get_destination_path(config):
+    return config['data']['destination_extracted_faces']
+
+
+def extract_face_from(image, destination_path, face_cascade, image_scale_factor=2, padding=50):
+    image, image_name = image
+    try:
+        new_height = int(image.shape[0] * image_scale_factor)
+        new_width = int(image.shape[1] * image_scale_factor)
+        image = cv.resize(image, (new_width, new_height), interpolation=cv.INTER_LANCZOS4)
+        gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray_image,
+                                              scaleFactor=1.1,
+                                              minNeighbors=5,
+                                              minSize=(50, 50),
+                                              flags=cv.CASCADE_SCALE_IMAGE)
+        num_face = 0
+        for (x, y, width, height) in faces:
+            cv.imwrite(os.path.join(destination_path + "/temp", "face" + str(num_face) + "_" + image_name),
+                       image[y - padding:y + height + padding, x - padding:x + width + padding])
+            num_face += 1
+    except:
+        pass
+
+
+def align_and_crop_faces_from(face, detector, predictor):
+    if face is not None:
+        face = align_face_from(face, detector, predictor)
+    if face is not None:
+        face = crop_face_from(face, detector, predictor)
+        return face
+    return None
+
+
+if __name__ == '__main__':
+    # Init parameters
+    with open('config.yml') as file:
+        config = yaml.full_load(file)
+    face_cascade = cv.CascadeClassifier(get_face_cascade_path(config))
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(get_predictor_path(config))
+    source_path = get_source_path(config)
+    destination_path = get_destination_path(config)
+    # Check directories
+    if not os.path.exists(source_path):
+        print("There are no images for detecting faces")
+        exit(1)
+    if not os.path.exists(destination_path):
+        os.makedirs(destination_path)
+    if not os.path.exists(destination_path + "/temp"):
+        os.makedirs(destination_path + "/temp")
+    # Detect faces
+    for image in tqdm(load_images_from(source_path), "Detect faces"):
+        extract_face_from(image, destination_path, face_cascade)
+    # Crop and align faces
+    for image in tqdm(load_images_from(destination_path + "/temp"), "Align and crop faces"):
+        face, image_name = image
+        face = align_and_crop_faces_from(face, detector, predictor)
+        # Store face
+        try:
+            cv.imwrite(os.path.join(destination_path, image_name), face)
+        except:
+            pass
+    # Delete temp folder
+    shutil.rmtree(destination_path + "/temp")
+    exit(0)
---
+++ FriendRecognize/utils/Filtering.py
@@ -0,0 +1,107 @@
+import copy
+import math
+
+import cv2 as cv
+import numpy as np
+
+
+def is_colored(image):
+    return len(image.shape) == 3
+
+
+def remove_color_from(image):
+    try:
+        if is_colored(image):
+            image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        return image
+    except:
+        return None
+
+
+def denoise_preserving_edge_from(image, epsilon=0.01, neighborhood_pixels=3):
+    try:
+        if is_colored(image):
+            cv.ximgproc.guidedFilter(image, image, neighborhood_pixels, epsilon, image)
+        else:
+            cv.ximgproc.guidedFilter(image, image, neighborhood_pixels, epsilon, image, -1)
+        return image
+    except:
+        return None
+
+
+def get_landmarks_from(image, detector, predictor):
+    faces = detector(image, 1)
+    for rect in faces:
+        return predictor(image, rect)
+
+
+def align_face_from(image, detector, predictor):
+    try:
+        shape = get_landmarks_from(image, detector, predictor)
+        x_sx = (shape.part(36).x + shape.part(37).x + shape.part(38).x +
+                shape.part(39).x + shape.part(40).x + shape.part(41).x) / 6
+        y_sx = (shape.part(36).y + shape.part(37).y + shape.part(38).y +
+                shape.part(39).y + shape.part(40).y + shape.part(41).y) / 6
+        x_dx = (shape.part(42).x + shape.part(43).x + shape.part(44).x +
+                shape.part(45).x + shape.part(46).x + shape.part(47).x) / 6
+        y_dx = (shape.part(42).y + shape.part(43).y + shape.part(44).y +
+                shape.part(45).y + shape.part(46).y + shape.part(47).y) / 6
+        angular_coefficient = math.atan((y_dx - y_sx) / (x_dx - x_sx))
+        height, width = image.shape[:2]
+        center = (width / 2, height / 2)
+        rotate_matrix = cv.getRotationMatrix2D(center=center, angle=math.degrees(angular_coefficient), scale=1)
+        return cv.warpAffine(src=image, M=rotate_matrix, dsize=(width, height))
+    except:
+        return None
+
+
+def crop_by_landmarks(image, landmarks, range_face, landmark_y_min, landmark_y_max):
+    try:
+        under_face_bridge_x = []
+        under_face_bridge_y = []
+        for i in range_face:
+            under_face_bridge_x.append(landmarks[i][0])
+            under_face_bridge_y.append(landmarks[i][1])
+        x_min = min(under_face_bridge_x)
+        x_max = max(under_face_bridge_x)
+        y_min = landmarks[landmark_y_min][1]
+        y_max = landmarks[landmark_y_max][1]
+        return image[y_min:y_max, x_min:x_max]
+    except:
+        return None
+
+
+def crop_face_from(image, detector, predictor):
+    try:
+        landmarks = np.array(
+            [[coordinate.x, coordinate.y] for coordinate in predictor(image, detector(image)[0]).parts()])
+        range_face = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        landmark_y_min = 19
+        landmark_y_max = 8
+        return crop_by_landmarks(image, landmarks, range_face, landmark_y_min, landmark_y_max)
+    except:
+        return None
+
+
+def highlight_a_face_from(image, size):
+    try:
+        image = cv.resize(image, (size, size), cv.INTER_LANCZOS4)
+        if image is not None:
+            image = cv.equalizeHist(image)
+        return image
+    except:
+        return None
+
+
+def preprocessing(image, detector, predictor, size=200):
+    image = remove_color_from(image)
+    recovery_image = copy.deepcopy(image)
+    if image is not None:
+        image = denoise_preserving_edge_from(image)
+    if image is not None:
+        image = align_face_from(image, detector, predictor)
+    if image is not None:
+        image = crop_face_from(image, detector, predictor)
+    if image is None:
+        image = recovery_image
+    return highlight_a_face_from(image, size)

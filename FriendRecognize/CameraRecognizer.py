09237557ad91a600c07b50e24ee4e93d83d66955
Vincenzo Salvati <vincenzosalvati@hotmail.it>
2022-03-05 22:21:52 +0100

    Reorganize Test

--- FriendRecognize/CameraRecognizer.py
+++ FriendRecognize/CameraRecognizer.py
@@ -1,27 +1,31 @@
-import os
-
 import cv2 as cv
 import dlib
 import numpy as np
+import yaml
 
-from FriendRecognize.utils.CropAndFilter import preprocessing
-from utils.FaceAligner import FaceAligner
-from utils.classifier.FeatureClassifier import FeatureClassifier
-from utils.lbp.LBPFeature import LBP
+from FriendRecognize.utils.Filtering import preprocessing
+from FriendRecognize.utils.UsefulMethods import extraction_feature_LBP
+from utils.trainModelAndClassifier.Classifier import Classifier
 
 
-def extraction_feature_LBP(X):
-    lbp = LBP(numPoints=16, radius=4, num_bins=100, n_row=10, n_col=10)
-    x_new = []
-    for i in (X):
-        hist, _ = lbp.describe(i)
-        x_new.append(hist)
-    x_new = np.array(x_new)
-    return x_new
+def get_model(config, feature):
+    return config['models'][feature]
+
+
+def get_features(config):
+    return config['features']['with']
+
+
+def get_predictor_path(config):
+    return config['libs']['predictor']
+
+
+def get_face_cascade_path(config):
+    return config['libs']['face_cascade']
 
 
 def compute_feature_for_testing(X_val):
-    X_val = extraction_feature_LBP(X_val)
+    X_val = extraction_feature_LBP(X_val, "")
     return X_val
 
 
@@ -35,22 +39,20 @@ def extract_features_for_testing(img, detector, predictor):
 
 
 if __name__ == '__main__':
-    model_vincenzo = FeatureClassifier()
-    model_vincenzo.load(os.path.join('models', 'Vincenzo'))
-    model_angelo = FeatureClassifier()
-    model_angelo.load(os.path.join('models', 'Angelo'))
-    model_dima = FeatureClassifier()
-    model_dima.load(os.path.join('models', 'Dima'))
-    model_giovanna = FeatureClassifier()
-    model_giovanna.load(os.path.join('models', 'Giovanna'))
-    model_noemi = FeatureClassifier()
-    model_noemi.load(os.path.join('models', 'Noemi'))
+    # Init parameters
+    with open('config.yml') as file:
+        config = yaml.full_load(file)
     detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor('libs/shape_predictor_68_face_landmarks.dat')
-    fa = FaceAligner(predictor, desiredFaceWidth=224, desiredFaceHeight=224)
-    face_cascade = cv.CascadeClassifier('libs/haarcascade_frontalface_default.xml')
+    predictor = dlib.shape_predictor(get_predictor_path(config))
+    face_cascade = cv.CascadeClassifier(get_face_cascade_path(config))
+    models = []
+    for feature in get_features(config):
+        model = Classifier()
+        model.load(get_model(config, feature))
+        models.append(model)
+    padding = 50
+
     video_capture = cv.VideoCapture(0)
-    padding = 0
     while True:
         ret, frames = video_capture.read()
         gray = cv.cvtColor(frames, cv.COLOR_BGR2GRAY)
@@ -58,38 +60,33 @@ if __name__ == '__main__':
             gray,
             scaleFactor=1.1,
             minNeighbors=5,
-            minSize=(150, 150),
+            minSize=(100, 100),
             flags=cv.CASCADE_SCALE_IMAGE
         )
+
         for (x, y, w, h) in faces:
-            image = frames[y - padding:y + h + padding, x - padding:x + w + padding]
-            X = extract_features_for_testing(image, detector, predictor)
+            X = extract_features_for_testing(frames[y - padding:y + h + padding, x - padding:x + w + padding],
+                                             detector,
+                                             predictor)
+            classifier_probabilities = []
             if X is not None and hasattr(X, 'shape'):
                 if len(X.shape) == 2:
-                    classifier_probability_vincenzo = round(model_vincenzo.predict_proba(X)[0][1], 3)
-                    classifier_probability_angelo = round(model_angelo.predict_proba(X)[0][1], 3)
-                    classifier_probability_dima = round(model_dima.predict_proba(X)[0][1], 3)
-                    classifier_probability_giovanna = round(model_giovanna.predict_proba(X)[0][1], 3)
-                    classifier_probability_noemi = round(model_noemi.predict_proba(X)[0][1], 3)
-                    max_probability = max(classifier_probability_vincenzo,
-                                          classifier_probability_angelo,
-                                          classifier_probability_dima,
-                                          classifier_probability_giovanna,
-                                          classifier_probability_noemi)
+                    i = 0
+                    max_probability = 0
+                    for i in range(len(get_features(config))):
+                        probability = round(models[i].predict_proba(X)[0][1], 3)
+                        classifier_probabilities.append(probability)
+                        if probability > max_probability:
+                            max_probability = probability
                 else:
-                    classifier_probability_vincenzo = 0
-                    classifier_probability_angelo = 0
-                    classifier_probability_dima = 0
-                    classifier_probability_giovanna = 0
-                    classifier_probability_noemi = 0
+                    for feature in get_features(config):
+                        classifier_probabilities.append(0)
                     max_probability = 0
             else:
-                classifier_probability_vincenzo = 0
-                classifier_probability_angelo = 0
-                classifier_probability_dima = 0
-                classifier_probability_giovanna = 0
-                classifier_probability_noemi = 0
+                for feature in get_features(config):
+                    classifier_probabilities.append(0)
                 max_probability = 0
+
             if max_probability < 0.5:
                 cv.rectangle(frames, (x, y), (x + w, y + h), (169, 169, 169), 2)
                 cv.rectangle(frames, (x, y), (x + w, y - 30), (255, 255, 255), -1)
@@ -100,66 +97,66 @@ if __name__ == '__main__':
                            0.6,
                            (169, 169, 169),
                            2)
-            elif classifier_probability_vincenzo >= max(classifier_probability_angelo,
-                                                        classifier_probability_dima,
-                                                        classifier_probability_giovanna,
-                                                        classifier_probability_noemi):
+            elif classifier_probabilities[0] >= max(classifier_probabilities[1],
+                                                    classifier_probabilities[2],
+                                                    classifier_probabilities[3],
+                                                    classifier_probabilities[4]):
                 cv.rectangle(frames, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 cv.rectangle(frames, (x, y), (x + w, y - 30), (255, 255, 255), -1)
                 cv.putText(frames,
-                           "Vincenzo " + str(round(classifier_probability_vincenzo * 100, 1)) + "%",
+                           "Vincenzo " + str(round(classifier_probabilities[0] * 100, 1)) + "%",
                            (x, y - 10),
                            cv.FONT_HERSHEY_SIMPLEX,
                            0.6,
                            (255, 0, 0),
                            2)
-            elif classifier_probability_angelo >= max(classifier_probability_vincenzo,
-                                                      classifier_probability_dima,
-                                                      classifier_probability_giovanna,
-                                                      classifier_probability_noemi):
+            elif classifier_probabilities[1] >= max(classifier_probabilities[0],
+                                                    classifier_probabilities[2],
+                                                    classifier_probabilities[3],
+                                                    classifier_probabilities[4]):
                 cv.rectangle(frames, (x, y), (x + w, y + h), (124, 252, 0), 2)
                 cv.rectangle(frames, (x, y), (x + w, y - 30), (255, 255, 255), -1)
                 cv.putText(frames,
-                           "Angelo " + str(round(classifier_probability_angelo * 100, 1)) + "%",
+                           "Angelo " + str(round(classifier_probabilities[1] * 100, 1)) + "%",
                            (x, y - 10),
                            cv.FONT_HERSHEY_SIMPLEX,
                            0.6,
                            (124, 252, 0),
                            2)
-            elif classifier_probability_dima >= max(classifier_probability_vincenzo,
-                                                    classifier_probability_angelo,
-                                                    classifier_probability_giovanna,
-                                                    classifier_probability_noemi):
+            elif classifier_probabilities[2] >= max(classifier_probabilities[0],
+                                                    classifier_probabilities[1],
+                                                    classifier_probabilities[3],
+                                                    classifier_probabilities[4]):
                 cv.rectangle(frames, (x, y), (x + w, y + h), (255, 123, 25), 2)
                 cv.rectangle(frames, (x, y), (x + w, y - 30), (255, 255, 255), -1)
                 cv.putText(frames,
-                           "Dima " + str(round(classifier_probability_dima * 100, 1)) + "%",
+                           "Dima " + str(round(classifier_probabilities[2] * 100, 1)) + "%",
                            (x, y - 10),
                            cv.FONT_HERSHEY_SIMPLEX,
                            0.6,
                            (255, 123, 25),
                            2)
-            elif classifier_probability_giovanna >= max(classifier_probability_vincenzo,
-                                                        classifier_probability_angelo,
-                                                        classifier_probability_dima,
-                                                        classifier_probability_noemi):
+            elif classifier_probabilities[3] >= max(classifier_probabilities[0],
+                                                    classifier_probabilities[1],
+                                                    classifier_probabilities[2],
+                                                    classifier_probabilities[4]):
                 cv.rectangle(frames, (x, y), (x + w, y + h), (255, 255, 0), 2)
                 cv.rectangle(frames, (x, y), (x + w, y - 30), (255, 255, 255), -1)
                 cv.putText(frames,
-                           "Giovanna " + str(round(classifier_probability_giovanna * 100, 1)) + "%",
+                           "Giovanna " + str(round(classifier_probabilities[3] * 100, 1)) + "%",
                            (x, y - 10),
                            cv.FONT_HERSHEY_SIMPLEX,
                            0.6,
                            (255, 255, 0),
                            2)
-            elif classifier_probability_noemi >= max(classifier_probability_vincenzo,
-                                                     classifier_probability_angelo,
-                                                     classifier_probability_dima,
-                                                     classifier_probability_giovanna):
+            elif classifier_probabilities[4] >= max(classifier_probabilities[0],
+                                                    classifier_probabilities[1],
+                                                    classifier_probabilities[2],
+                                                    classifier_probabilities[3]):
                 cv.rectangle(frames, (x, y), (x + w, y + h), (128, 0, 128), 2)
                 cv.rectangle(frames, (x, y), (x + w, y - 30), (255, 255, 255), -1)
                 cv.putText(frames,
-                           "Noemi " + str(round(classifier_probability_noemi * 100, 1)) + "%",
+                           "Noemi " + str(round(classifier_probabilities[4] * 100, 1)) + "%",
                            (x, y - 10),
                            cv.FONT_HERSHEY_SIMPLEX,
                            0.6,
